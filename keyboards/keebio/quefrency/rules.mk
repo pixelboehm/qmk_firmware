7eb8a12f1720b572869368deacf8f2bf2a8b8f50
Maximilian Fischer <maximilian@maxfs.de>
2024-01-18 13:55:24 +0100

    allow dynamic makros

--- keyboards/keebio/quefrency/rules.mk
+++ keyboards/keebio/quefrency/rules.mk
@@ -16,6 +16,7 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
+DYNAMIC_MACRO_ENABLE = yes
 
 SPLIT_KEYBOARD = yes
 
